12c624c31fa2afd3ff735c4cf1843f93bd83a5ab
Devin <60894239+NNyaklir@users.noreply.github.com>
2025-03-17 22:21:42 -0600

    updated ProjectManager, added example.json
    added load function to project manager and created an example json file for project information storage

--- ProjectManager.py
+++ ProjectManager.py
@@ -2,6 +2,7 @@ import json
 import os
 from datetime import datetime
 
+PROJECT_FILE="example.json" ##subject to change
 
 ## create project 
 def createProject (projectID,name,target,methods):
@@ -20,4 +21,11 @@ def createProject (projectID,name,target,methods):
 def saveProject(data,filename="project.json"):
     with open(filename,"w") as f:
         json.dump(data, f, indent=4)
-        
+    
+## load feature    
+def loadProject():
+    if not os.path.exisits(PROJECT_FILE):
+        return{"error":"No Project found. Please create one, or load another"}
+    with open(PROJECT_FILE, "r") as file:
+        return json.load(file)
+
